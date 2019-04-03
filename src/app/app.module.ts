e304bf4329a4d48441502b0256c2e26a89e2af12
DanielaAlvarado <A01329233@itesm.mx>
2019-04-03 12:29:27 -0600

    Revert "todos feature"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { VoterComponent } from './voter/voter.component';
-import { TodosComponent } from './todos/todos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    VoterComponent,
-    TodosComponent
+    VoterComponent
   ],
   imports: [
     BrowserModule,
