ac77ca8ca5b54359845d191d438da966ec43837e
Daniela Alvarado Pereda <danielaalvaradopereda@hotmail.com>
2019-04-05 18:09:06 -0600

    user feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+/* New Dev 3 */
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -5,12 +6,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { VoterComponent } from './voter/voter.component';
 import { TodosComponent } from './todos/todos.component';
+import { UsersComponent } from './users/users.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     VoterComponent,
-    TodosComponent
+    TodosComponent,
+    UsersComponent,
+    UserDetailsComponent
   ],
   imports: [
     BrowserModule,
